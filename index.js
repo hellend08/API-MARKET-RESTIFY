ef49f538b2afd257bdc8c71ea787df8538b573c9
MAYTESI <mayte16inga@gmail.com>
2019-10-23 20:35:40 -0500

    implementando restify en index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const restify = require('restify');
+const userRoutes = require('./src/users-routes')
+
+const server = restify.createServer();
+const PORT = 3000;
+
+//settings
+server.use(restify.plugins.acceptParser(server.acceptable))
+server.use(restify.plugins.queryParser())
+server.use(restify.plugins.bodyParser())
+userRoutes.applyRoutes(server);
+
+server.listen(PORT,()=>{
+    console.log(`listen on ${server.name}`, server.url);
+})

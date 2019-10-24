3852527c7efc1aca99f6136ccfa4b14cd2477b6e
MAYTESI <mayte16inga@gmail.com>
2019-10-23 21:09:29 -0500

    configuracion de userRoutes en index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const restify = require('restify');
-//const userRoutes = require('./src/users-routes')
+const userRoutes = require('./main/routes/users/usersRoutes')
 
 const server = restify.createServer();
 const PORT = 4000;
@@ -8,7 +8,7 @@ const PORT = 4000;
 server.use(restify.plugins.acceptParser(server.acceptable))
 server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
-//userRoutes.applyRoutes(server);
+userRoutes.applyRoutes(server);
 
 server.listen(PORT,()=>{
     console.log(`listen on ${server.name}`, server.url);

9ec18112fa64c69f04571a398ff13ceb89e0f057
MAYTESI <mayte16inga@gmail.com>
2019-10-23 20:52:29 -0500

    subiendo base de datos estatica  de usuarios y productos

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const restify = require('restify');
-const userRoutes = require('./src/users-routes')
+//const userRoutes = require('./src/users-routes')
 
 const server = restify.createServer();
-const PORT = 3000;
+const PORT = 4000;
 
 //settings
 server.use(restify.plugins.acceptParser(server.acceptable))
 server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
-userRoutes.applyRoutes(server);
+//userRoutes.applyRoutes(server);
 
 server.listen(PORT,()=>{
     console.log(`listen on ${server.name}`, server.url);
---
+++ main/Bd/db.js
@@ -0,0 +1,28 @@
+module.exports.users = [
+  {
+    id:'123456789',
+    name:'Mayte',
+    email:'mayte@gmail.com',
+    password:'noveasmipassword'
+  },
+  {
+    id:'987654321',
+    name:'Helen',
+    email:'helen@gmail.com',
+    password:'helen123'
+  }
+];
+module.exports.products =[
+  {
+    name:'casaca',
+    price:'80',
+    currency:'USD',
+    owner:'123456789'
+  },
+  {
+    name:'zapatillas',
+    price:'120',
+    currency:'USD',
+    owner:'987654321'
+  }
+]
